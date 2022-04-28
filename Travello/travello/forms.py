b80ef83950d9399860b30c5004a1cd8c213e735d
Monish Khatri <monish.k@int.biztechcs.com>
2022-04-28 12:04:59 +0530

    design

--- Travello/travello/forms.py
+++ Travello/travello/forms.py
@@ -4,11 +4,11 @@ from .models import Destination
 
 class DestinationForm(forms.ModelForm):
     # each field would be mapped as an input field in HTML
-    name = forms.CharField(label="Name",max_length=100, min_length=10,required=True,widget=forms.TextInput(attrs={"class":"class-1 class-2"}))
-    image = forms.ImageField(allow_empty_file=True,required=False)
-    desc = forms.CharField(max_length=500,widget=forms.Textarea())
-    price = forms.IntegerField(max_value=5000,min_value=500,required=True)
-    offer = forms.BooleanField(required=False)
+    # name = forms.CharField(label="Name",max_length=100, min_length=10,required=True,widget=forms.TextInput(attrs={"class":"class-1 class-2"}))
+    # image = forms.ImageField(allow_empty_file=True,required=False)
+    # desc = forms.CharField(max_length=500,widget=forms.Textarea())
+    # price = forms.IntegerField(max_value=5000,min_value=500,required=True)
+    # offer = forms.BooleanField(required=False)
 
     class Meta:
         model = Destination
