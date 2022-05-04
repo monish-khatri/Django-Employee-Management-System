84cf5c6b38968a9fd682724d0011e7c0313c9416
Tarang <tarang.panchal@biztechcs.com>
2022-05-04 16:30:21 +0530

    added tags table

--- web_app/blog/models.py
+++ web_app/blog/models.py
@@ -9,6 +9,18 @@ class Category(models.Model):
     def __str__(self):
         return self.cat_name
 
+class Tags(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tag_name = models.CharField(max_length=250)
+    tag_status = models.IntegerField(default='1')
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Tags'
+
+    def __str__(self):
+        return self.tag_name
+
 class Blog(models.Model):
     blog_id = models.BigAutoField(primary_key=True)
     user_id = models.IntegerField(default='1')
@@ -16,6 +28,7 @@ class Blog(models.Model):
     blog_title = models.CharField(max_length=250)
     blog_desc = models.TextField()
     blog_img = models.ImageField(upload_to="blog/images", default='untitled.png')
+    blog_tags = models.ManyToManyField(Tags)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -29,7 +42,7 @@ class Blog(models.Model):
             
     @property
     def blog_with_cat(self):
-        return self.blog_title + '- asdd'
+        return self.blog_title
 
 class Contact(models.Model):
     id = models.AutoField(primary_key=True)
