d785b02e18fc91a743493b4f1749974874964529
Monish Khatri <monish.k@int.biztechcs.com>
2022-05-06 12:34:17 +0530

    form change

--- Crud/crud_practice/employee/forms.py
+++ Crud/crud_practice/employee/forms.py
@@ -10,13 +10,13 @@ class EmployeeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EmployeeForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
-            fieldClasses = 'form-control phone-class' if visible.name == 'phone' else 'form-control'
+            fieldClasses = 'form-control'
             visible.field.widget.attrs['class'] = fieldClasses
 
 
     class Meta:
         model = Employee
-        fields = ['name', 'email','phone','image']
+        fields = ['name', 'email','phone','image','date_of_birth','team']
     name = forms.CharField(max_length=50,required=True,widget=forms.TextInput(attrs={'placeholder':'Name','id':'name'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Email Address','id':'email'}),required=True)
     phone = forms.IntegerField(min_value=10000000,max_value=999999999999,required=True,widget=forms.NumberInput(attrs={'placeholder':'Phone Number','id':'phone'}))
