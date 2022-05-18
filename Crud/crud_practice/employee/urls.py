e8dcd5f17fd417f029c65d04439e66e3a0eb6626
Monish Khatri <monish.k@int.biztechcs.com>
2022-05-18 10:45:08 +0530

    Redirect View

--- Crud/crud_practice/employee/urls.py
+++ Crud/crud_practice/employee/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required,user_passes_test
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', login_required(views.EmployeeView.as_view(),'','login')),
-    path('admins/', login_required(views.AdminView.as_view(),'','login')),
-    path('teams/',  login_required(views.TeamView.as_view(),'','login')),
+    path('', login_required(views.EmployeeView.as_view(),'','login'),name=""),
+    path('admins/', login_required(views.AdminView.as_view(),'','login'),name="admins"),
+    path('teams/',  login_required(views.TeamView.as_view(),'','login'),name="teams"),
     path('edit/<int:id>', login_required(views.EmployeeView.as_view(is_edit = True),'','login')),
     path('edit_admin/<int:id>', login_required(views.AdminView.as_view(is_edit = True),'','login')),
     path('edit_team/<int:id>', login_required(views.TeamView.as_view(is_edit = True),'','login')),
@@ -13,6 +14,7 @@ urlpatterns = [
     path('delete_admin/<str:str>', login_required(views.AdminView.as_view(),'','login')),
     path('team_delete/<str:str>', login_required(views.TeamView.as_view(),'','login')),
     path('team/<int:id>',login_required(views.TeamEmployeeView.as_view(),'','login')),
-    path('about-us/', login_required(views.AboutUsView.as_view(),'','login')),
-    path('profile/', login_required(views.ProfileView.as_view(),'','login')),
+    path('about-us/', login_required(views.AboutUsView.as_view(),'','login'),name="about-us"),
+    path('profile/', login_required(views.ProfileView.as_view(),'','login'),name="profile"),
+    path('redirect-view/', RedirectView.as_view(url='https://www.djangoproject.com/')),
 ]
