5e2264e3bfa4f66511fbd4a68dfef51eec3574df
Monish Khatri <khatrimk10@gmail.com>
2022-11-17 22:38:10 +0530

    Update settings.py

--- Crud/crud_practice/crud_practice/settings.py
+++ Crud/crud_practice/crud_practice/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_crud',
         'USER': 'root',
-        'PASSWORD': 'M0nish@10',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
